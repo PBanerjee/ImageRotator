da6acd5f50b6e2fa395f5d2a91305821a8353615
Pallab Banerjee <pallabacsrdeveloper@gmail.com>
2013-05-06 18:50:53 +0530

    Basic rotation achieved
    XML parsed
    Style Mapped
    Base CSS added
    Dynamic rotation (show/hide) added.

--- public_html/js/ImageRotator.js
+++ public_html/js/ImageRotator.js
@@ -21,8 +21,11 @@
             if(!this.listeners[eventType]){
                 this.listeners[eventType] = {
                     eventType: eventType,
-                    callback: callback
-                }
+                    callback: [callback]
+                };
+            }else{
+                //add to the existing list of callbacks
+                this.listeners[eventType].callback.push(callback);
             }
         },
 
@@ -42,10 +45,14 @@
         dispatchEvent: function(eventType){
             //get the obj from the listeners hashTable
             var evtObj = this.listeners[eventType],
-                callback = evtObj && evtObj.callback;
+                callback = evtObj && evtObj.callback,
+                i = 0,
+                numCallbcks = callback.length;
 
-            if(callback){
-                callback.call(this.scope);
+            for(i; i < numCallbcks; i += 1){
+                if(callback[i]){
+                    callback[i].call(this.scope);
+                }
             }
         }
     };
@@ -61,19 +68,29 @@
      * @param   data    The object which contains the data as XML
      *                  or the url for the XML.
      */
-    var ImageRotator = function(data){
+    var ImageRotator = function(data, containerDivID){
         //list of slides
         this.slides = [];
+        //list of styles
+        this.styles = [];
+        //list of slide elements as a batch
+        this.slideElems = [];
         //need to load or parse the data.
         this.dataObj = data;
         //start data fectching or parsing
         this.dataObj && this.getData();
         //subscribe to the event object
         this.event = new Event(this);
-    };
-
-    //default options
-    ImageRotator.defaults = {
+        //store the container div
+        //@todo: need to auto detect the # sign.
+        this.container = $("#"+containerDivID);
+        //default options
+        this.defaults = {
+            speed: 500, //ms
+            stay: 5000,
+            animating: false,
+            currentSlide: 0
+        };
 
     };
 
@@ -85,7 +102,8 @@
 
         parseXML: function(xmlData){
             var xmlDoc,
-                slides = this.slides || [];
+                slides = this.slides || [],
+                styles = this.styles || [];
             //check whether the passed data is in XML format
             //remove white spaces (trim)
             if(xmlData.childNodes){
@@ -113,13 +131,36 @@
 
                     if(nodeName === 'image' || nodeName === 'heading'
                                         || nodeName === 'description'){
-                        slide[nodeName] = $(node).text();
+                        //check if any style is assigned
+                        slide[nodeName] = {
+                            styleId: $(node).attr("styleId"),
+                            type: $(node).attr("type")
+                        }
+                        //nodeName is used to store the value
+                        slide[nodeName][nodeName] = $(node).text();
                     }
                 }
                 slides.push(slide);
             });
             //update class level sildes object
             this.slides = slides;
+
+            //iterate over the style object and store data
+            $(xmlDoc).find("style").each(function(){
+                var style = {},
+                    attrMap = this.attributes;
+
+                $.each(attrMap, function(){
+                    style[this.nodeName] = this.nodeValue;
+                });
+                //for easy reference
+                styles[style.id] = style;
+
+                styles.push(style);
+            });
+            //update class level style object
+            this.styles = styles;
+
             //fire XML parse complete event
             this.event.dispatchEvent('xml-parsed');
         },
@@ -156,10 +197,164 @@
                    }
                 });
             }
+        },
+
+        /*
+         * function render content
+         */
+        render: function(){
+            var imageContainer = $('<div id="imageContainer"></div>'),
+                ulElem = $('<ul></ul>'),
+                textContainer = $('<div id="textContainer"></div>'),
+                i = 0,
+                slides = this.slides,
+                numSlides = slides && slides.length,
+                slideObj,
+                imgObj,
+                imgSrc,
+                imgStyleId,
+                imgAltText,
+                imgElem,
+                headingObj,
+                headingType,
+                headingStyleId,
+                heading,
+                headerElem,
+                descObj,
+                descType,
+                descStyleId,
+                description,
+                descElem;
+
+             //append the div for the container of images
+            this.container.append(imageContainer);
+            //append the UL element to the image container.
+            imageContainer.append(ulElem);
+            //append the div for the container of heading & descriptions
+            this.container.append(textContainer);
+            //now dynimically create the images element based
+            //on the slides
+            for(i; i < numSlides; i += 1){
+                slideObj = slides[i];
+                //image
+                imgObj = slideObj.image;
+                imgSrc = imgObj.image;
+                imgStyleId = imgObj.styleId;
+                imgAltText = slideObj.altText || "image"+i;
+                //create the image lement
+                imgElem = $("<img src='"+imgSrc+"' alt='"+imgAltText+"' />");
+                //now apply the style property is defined, apply to this elem
+                imgStyleId && this.applyStyles(imgElem, imgStyleId);
+                //append to the UL element
+                ulElem.append(imgElem);
+                //initially hide them
+                imgElem.hide();
+
+                //heading
+                headingObj = slideObj.heading;
+                headingType = headingObj && headingObj.type;
+                headingStyleId = headingObj && headingObj.styleId;
+                heading = headingObj && headingObj.heading;
+                //create the header element
+                headerElem = $("<"+headingType+">"+heading+"</"+headingType+">");
+                //apply style
+                headingStyleId && this.applyStyles(headerElem, headingStyleId);
+                //append to the div
+                textContainer.append(headerElem);
+                //initially hide
+                headerElem.hide();
+
+                //description
+                descObj = slideObj.description;
+                descType = descObj && descObj.type;
+                descStyleId = descObj && descObj.styleId;
+                description = descObj && descObj.description;
+                //create the description element
+                descElem = $("<"+descType+">"+description+"</"+descType+">");
+                //apply style
+                descStyleId && this.applyStyle(descElem, descStyleId);
+                //append
+                textContainer.append(descElem);
+                //initially hide
+                descElem.hide();
+
+                //add to the global slide Elements array
+                this.slideElems[i] = {
+                    image_element: imgElem,
+                    header_element: headerElem,
+                    desc_element: descElem
+                };
+            }
+
+            //fire the creation complete event
+            this.event.dispatchEvent('creation-complete');
+        },
+
+        applyStyles: function(element, imgStyleId){
+            //get the style object from the styles array
+            var styleObj = this.styles[imgStyleId];
+            if(styleObj){
+                for(var prop in styleObj){
+                    $(element).css(prop, styleObj[prop]);
+                }
+            }
+        },
+
+        startRotate: function(){
+            //put in a loop
+            var scope = this;
+            setTimeout( function(){
+                scope.hidePrevSlide.call(scope);
+                scope.showNextSlide.call(scope);
+            }, this.defaults.speed);
+        },
+
+        hidePrevSlide: function(){
+            var prevSlide = this.defaults.currentSlide - 1,
+                numSlides = this.slideElems && this.slideElems.length,
+                slideObj;
+
+            //if there is no current slide nothing to do
+            if((prevSlide != 0 && prevSlide < 0) ||
+                            prevSlide >= numSlides){
+                return;
+            }
+
+            //hide the current elements
+            slideObj = this.slideElems[prevSlide];
+
+            //hide all elements
+            $(slideObj.image_element).hide();
+            $(slideObj.header_element).hide();
+            $(slideObj.desc_element).hide();
+        },
+
+        showNextSlide: function(){
+            var currentSlide = this.defaults.currentSlide ||
+                                (this.defaults.currentSlide = 0),
+                numSlides = this.slideElems && this.slideElems.length,
+                slideObj,
+                scope = this;
+
+            if(currentSlide >= numSlides){
+               currentSlide = 0
+            }
+
+            slideObj = this.slideElems[currentSlide];
+
+            //show all elements
+            $(slideObj.image_element).show();
+            $(slideObj.header_element).show();
+            $(slideObj.desc_element).show();
+
+             //increase the counter
+            this.defaults.currentSlide = currentSlide + 1;
+
+            //call next slide after the stay duration
+            setTimeout(function(){
+                scope.startRotate.call(scope);
+            }, this.defaults.stay);
         }
-
-
-
     };
 
     //expose to the global scope
